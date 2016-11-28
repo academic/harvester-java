b4817fa7afd0f6da4af60c4a00a9b8d0285f2b16
Hüseyin Mert <huseyinin@gmail.com>
2016-11-28 10:34:02 +0300

    Steps generated #5

--- src/main/java/io/academic/service/CrawlerService.java
+++ src/main/java/io/academic/service/CrawlerService.java
@@ -30,11 +30,11 @@ public class CrawlerService {
 
     Logger log = LoggerFactory.getLogger( CrawlerService.class );
 
-    public void parse(CrawlerDao crawlerDao){
+    public void parse(CrawlerDao crawlerDao) {
 
         log.info( "URL crawler started href: {}", crawlerDao.getHref() );
 
-
+        String resumptionToken = null;
         Parser parser = new DcXMLParser();
         ContentHandler handler = new BodyContentHandler();
         Metadata metadata = new Metadata();
@@ -42,18 +42,34 @@ public class CrawlerService {
 
 
         try {
-            String resumptionToken =this.getResumptionToken( crawlerDao.getHref());
-            log.info( "URL crawler try token: {}", resumptionToken );
+
+            // Step 1: Get url
+
+            Document doc = Jsoup.connect( crawlerDao.getHref() ).get();
+
+            // Step 2: Get records of this page
+
+            // Step 3: Parse DC
+
+            // Step 4: Record OAI Records to database
+
+            // Step 5: Get resumptionToken
+
+            // Step 6: Built new url
+
+            resumptionToken = this.getResumptionToken(doc);
+            log.info( "URL crawler will try new token: {}", resumptionToken );
+
+            // Step 7: Recall this metod with new link
+
         } catch (IOException e) {
             e.printStackTrace();
         }
 
     }
 
-    public String getResumptionToken(String Link) throws IOException {
-
-        Document doc = Jsoup.connect(Link).get();
-        return doc.select("resumptionToken").first().ownText();
+    public String getResumptionToken(Document doc) throws IOException {
+        return doc.select( "resumptionToken" ).first().ownText();
     }
 
 
