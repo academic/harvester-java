3823e088c0713efd8f8bec71928c56d1c19c56a6
Hüseyin Mert <huseyinin@gmail.com>
2016-11-28 10:08:40 +0300

    resumptionToken implemented #5

--- src/main/java/io/academic/service/CrawlerService.java
+++ src/main/java/io/academic/service/CrawlerService.java
@@ -3,7 +3,24 @@ package io.academic.service;
 import io.academic.dao.CrawlerDao;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.apache.tika.exception.TikaException;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.parser.ParseContext;
+import org.apache.tika.parser.Parser;
+import org.apache.tika.parser.xml.DcXMLParser;
+import org.apache.tika.sax.BodyContentHandler;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.springframework.stereotype.Service;
+
+import org.xml.sax.ContentHandler;
+import org.xml.sax.SAXException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
 
 /**
  * Crawls xmls and sends to data management services
@@ -13,16 +30,30 @@ public class CrawlerService {
 
     Logger log = LoggerFactory.getLogger( CrawlerService.class );
 
+    public void parse(CrawlerDao crawlerDao){
 
-    public void parse(CrawlerDao crawlerDao) {
-
-        log.info( "URL crawler started", crawlerDao.getHref() );
+        log.info( "URL crawler started href: {}", crawlerDao.getHref() );
 
 
-        /**
-         * Parse url
-         */
+        Parser parser = new DcXMLParser();
+        ContentHandler handler = new BodyContentHandler();
+        Metadata metadata = new Metadata();
+        ParseContext parseContext = new ParseContext();
 
+
+        try {
+            String resumptionToken =this.getResumptionToken( crawlerDao.getHref());
+            log.info( "URL crawler try token: {}", resumptionToken );
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public String getResumptionToken(String Link) throws IOException {
+
+        Document doc = Jsoup.connect(Link).get();
+        return doc.select("resumptionToken").first().ownText();
     }
 
 
